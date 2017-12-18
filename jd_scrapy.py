9695b63a14053fc8faffd0ab347ad1ab7bdf00f5
NoeYoung <noe@live.cn>
2017-12-18 20:39:10 +0800

    Initial commit

---
+++ jd_scrapy.py
@@ -0,0 +1,17 @@
+import requests
+import requests.packages.urllib3
+
+class pyJD(object):
+
+    def __init__(self):
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+            'ContentType': 'text/html; charset=utf-8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+        }
+
+
+if __name__ == '__main__':
+    print('hello world')
